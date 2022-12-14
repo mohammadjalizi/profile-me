a9b7c8dff355d4a105d9595a9201b2f8981839b3
mohammadjalizi <mjalizi1376@gmail.com>
2022-12-14 14:15:49 +0330

    project

---
+++ src/navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'   
+import images from '../navbar/pic.jpeg'
+
+function Navbar() {
+  return (
+    <div className=' bg-black text-white'>
+<div className=' navbar'>
+
+<div>
+    <img src={images}/>
+    
+     </div>
+
+
+
+</div>
+
+
+
+    </div>
+  )
+}
+
+export default Navbar
