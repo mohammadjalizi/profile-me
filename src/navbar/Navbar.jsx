15b64bc56c3bff28df148789d91f23872996e62d
mohammadjalizi <mjalizi1376@gmail.com>
2022-12-14 14:45:18 +0330

    project

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -4,21 +4,20 @@ import images from '../navbar/pic.jpeg'
 function Navbar() {
   return (
     <div className=' bg-black text-white'>
-<div className=' navbar'>
+<div className=' navbar flex  justify-between container '>
 
 <div>
     <img  className=' w-[80px]  border  rounded-full ' src={images}/ >
     
      </div>
-<div>
-<ul>
+<div className=' flex items-center justify-center'>
+<ul className=' flex gap-x-4 '>
 
-    <li>درباره من</li>
-    <li>مهارت ها</li>
-    <li></li>
-    <li></li>
-</ul>
-
+    <li className=' cursor-pointer hover:border  hover:rounded-full hover:bg-gradient-to-r from-green-400 to-blue-500  p-2  translate transition ease-in duration-700'>درباره من</li>
+    <li className=' cursor-pointer hover:border  hover:rounded-full hover:bg-gradient-to-r from-green-400 to-blue-500   p-2  translate transition ease-in duration-700'>مهارت ها</li>
+    <li className=' cursor-pointer hover:border  hover:rounded-full hover:bg-gradient-to-r from-green-400 to-blue-500  p-2  translate transition ease-in duration-700'>تماس با من</li> 
+    <li className=' cursor-pointer hover:border  hover:rounded-full hover:bg-gradient-to-r from-green-400 to-blue-500  p-2  translate transition ease-in duration-700'> رزومه من(CV) </li>
+    </ul>
 
 </div>
 
